36949004715333ebe13401c313bb687e1072224b
brenosilvapedra <112491458+brenosilvapedra@users.noreply.github.com>
2023-09-19 09:57:28 -0300

    Add files via upload

---
+++ Alura-mid/main.js
@@ -0,0 +1,18 @@
+const pom = document.querySelector('.tecla_pom');
+const clap = document.querySelector('.tecla_clap');
+const tim = document.querySelector('.tecla_tim');
+
+const puff = document.querySelector9('.tecla_puff');
+const splash = document.querySelector('.tecla_splash');
+const toim = document.querySelector('.tecla_toim');
+
+const psh = document.querySelector('.tecla_psh');
+const tic = document.querySelector('.tecla_tic');
+const tom = document.querySelector('.tecla_tom');
+
+pom.addEventListener('click', function pom() {
+    document.querySelector('#som_tecla_pom').play();
+} )
+
+
+
