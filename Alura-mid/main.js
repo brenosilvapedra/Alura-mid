909ab99d9c845333147573e75e00c9b552b5819d
brenosilvapedra <112491458+brenosilvapedra@users.noreply.github.com>
2023-09-20 22:12:14 -0300

    Add files via upload

--- Alura-mid/main.js
+++ Alura-mid/main.js
@@ -1,23 +1,21 @@
-function Som (idAudio) {
-    document.querySelector(idAudio).play();
-}
-
-
-
 const teclas = document.querySelectorAll('.tecla'); // "Table" com todas as teclas
 
-let n = 0;
+var n = 0; // Declara a variavel n para ser usada na verificação
 
 while ( n < 9) {    // Aumenta o valor de n até 9
+    
+    function Som (idAudio) {
+        document.querySelector(idAudio).play();
+    }
     
     const instrumento = teclas[n].classList[1]; // Busca a segunda classe do elemento e retorna em string
 
 
-    teclas[n].onclick = function () {
+    teclas[n].onclick = function () { // Identifica o instrumento a ser tocado
         Som('#som_'+instrumento );
     }
     
-    n = n + 1;
+    n += 1; // Aumenta o valor de n de 1 em 1 para evitar um loop infinito
 
     console.log(n);
 }
