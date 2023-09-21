88942cc53c2a88cb4bead107b5b34ed6ef9e2c65
brenosilvapedra <112491458+brenosilvapedra@users.noreply.github.com>
2023-09-21 06:25:47 -0300

    Update main.js

--- Alura-mid/main.js
+++ Alura-mid/main.js
@@ -1,6 +1,6 @@
 const teclas = document.querySelectorAll('.tecla'); // "Table" com todas as teclas
 
-var n = 0; // Declara a variavel n para ser usada na verificação
+let n = 0; // Declara a variavel n para ser usada na verificação
 
 while ( n < 9) {    // Aumenta o valor de n até 9
     
