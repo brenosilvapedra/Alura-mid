50cca75b6b4f9e18af4fb19b6615c7f9e90dc7e1
brenosilvapedra <112491458+brenosilvapedra@users.noreply.github.com>
2023-11-30 00:30:53 -0300

    Update main.js

--- Alura-mid/main.js
+++ Alura-mid/main.js
@@ -1,23 +1,25 @@
-const teclas = document.querySelectorAll('.tecla'); // "Table" com todas as teclas
-
-let n = 0; // Declara a variavel n para ser usada na verificação
-
-while ( n < 9) {    // Aumenta o valor de n até 9
-    
-    function Som (idAudio) {
-        document.querySelector(idAudio).play();
-    }
-    
-    const instrumento = teclas[n].classList[1]; // Busca a segunda classe do elemento e retorna em string
-
-
-    teclas[n].onclick = function () { // Identifica o instrumento a ser tocado
-        Som('#som_'+instrumento );
-    }
-    
-    n += 1; // Aumenta o valor de n de 1 em 1 para evitar um loop infinito
-
-    console.log(n);
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
+ 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (){
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
